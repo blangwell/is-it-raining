415c6c3f1c2e867a606fff7312940eb71bccef44
blangwell <b.langwell@outlook.com>
2021-01-19 00:42:38 -0800

    Update api url and test

--- server.js
+++ server.js
@@ -11,25 +11,17 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 
 app.get('/', (req, res) => {
-  // axios call OpenWeatherAPI current weather data
   res.render('home');
 })
 
 app.get('/search', (req, res) => {
   console.log(req.query.location)
-  axios.get(`api.openweathermap.org/data/2.5/weather?q=${req.query.location}}&appid=${API_KEY}`)
+  axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${req.query.location}&APPID=${API_KEY}`)
   .then(response => {
-    console.log(response)
-    res.redirect('/', {data: response});
+    console.log(response.data.weather[0])
+    res.redirect('/');
   })
   .catch(err => console.log(err))
-  
-  // axios.get(`api.openweathermap.org/data/2.5/weather?q=${req.body.location}&appid=${API_KEY}`)
-  // .then(response => {
-  //   console.log(response)
-  //   res.redirect('results', {response: response});
-  // })
-  
 })
 
-app.listen(PORT || 3000, () => console.log(`server running on ${PORT}`)); 
+app.listen(PORT, () => console.log(`server running on ${PORT}`)); 
